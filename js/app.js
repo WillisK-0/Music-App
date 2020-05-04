b4a431c53b63c51726132491e2716d68feaa57ce
Keith Willis <williskeith04@gmail.com>
2020-05-04 13:45:15 -0500

    added apis and started template

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 let trackBox = document.getElementById("trackBox")
 let artistBox = document.getElementById("artistBox")
 let searchButton = document.getElementById("searchButton")
-
+let trackInfo = document.getElementById("trackInfo")
+let trackName = document.getElementById("trackName")
 // f86b06c5332c847e2a02380f28826dc2  <--- This is the API key for last.fm
 
 
@@ -9,11 +10,19 @@ searchButton.addEventListener("click", function () {
     let trackName = trackBox.value
     let artistName = artistBox.value
 
+    fetch(`http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=f86b06c5332c847e2a02380f28826dc2&artist=${artistName}&track=${trackName}&format=json`)
+    .then(r => r.json())
+    .then(trackAbout => {
+        console.log(trackAbout)
+        let head = `<h1></h1>`
+        trackName.innerHTML = head
+    })
 
     fetch(`https://api.lyrics.ovh/v1/${artistName}/${trackName}`)
         .then(response => response.json())
         .then(song => {
-            console.log(song.lyrics)
+            let item = `<p>${song.lyrics}</p>`
+            trackInfo.innerHTML = item
         })
 
     fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artistName}&api_key=f86b06c5332c847e2a02380f28826dc2&format=json`)
@@ -21,4 +30,8 @@ searchButton.addEventListener("click", function () {
     .then(artistInfo => {
         console.log(artistInfo)
     })
+
+
+
+    
 })
