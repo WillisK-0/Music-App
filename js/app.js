663aa61cc5f9079a95dd6fe1b1244b0faef680f5
Keith Willis <williskeith04@gmail.com>
2020-05-04 10:56:24 -0500

    added artist info

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ let trackBox = document.getElementById("trackBox")
 let artistBox = document.getElementById("artistBox")
 let searchButton = document.getElementById("searchButton")
 
+// f86b06c5332c847e2a02380f28826dc2  <--- This is the API key for last.fm
+
 
 searchButton.addEventListener("click", function () {
     let trackName = trackBox.value
@@ -13,4 +15,10 @@ searchButton.addEventListener("click", function () {
         .then(song => {
             console.log(song.lyrics)
         })
+
+    fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=${artistName}&api_key=f86b06c5332c847e2a02380f28826dc2&format=json`)
+    .then(results => results.json())
+    .then(artistInfo => {
+        console.log(artistInfo)
+    })
 })
